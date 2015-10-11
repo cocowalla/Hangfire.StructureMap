867b6665538e5a8f7cc6466332d7f71a47d3a18f
Lucas H. Byers <lucashby@live.com>
2015-10-11 03:34:56 -0400

    Updated project to take advantage of Hangfire 1.5 IoC improvements.

---
+++ src/Hangfire.StructureMap/GlobalConfigurationExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Hangfire.StructureMap
+{
+    using System;
+
+    using Hangfire.Annotations;
+
+    using global::StructureMap;
+
+    /// <summary>
+    /// Global Configuration extensions.
+    /// </summary>
+    public static class GlobalConfigurationExtensions
+    {
+        /// <summary>
+        /// Tells global configuration to use the specified StructureMap container as a job activator.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="container">The container.</param>
+        /// <returns>An instance of <see cref="IGlobalConfiguration{StructureMapJobActivator}"/>.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// configuration
+        /// or
+        /// container
+        /// </exception>
+        public static IGlobalConfiguration<StructureMapJobActivator> UseStructureMapActivator([NotNull] this IGlobalConfiguration configuration, [NotNull] IContainer container)
+        {
+            if (configuration == null) throw new ArgumentNullException("configuration");
+            if (container == null) throw new ArgumentNullException("container");
+
+            return configuration.UseActivator(new StructureMapJobActivator(container));
+        }
+    }
+}
--- src/Hangfire.StructureMap/StructureMapBootstrapperConfigurationExtensions.cs
+++ src/Hangfire.StructureMap/StructureMapBootstrapperConfigurationExtensions.cs
@@ -1,14 +1,20 @@
-﻿using StructureMap;
-
-namespace Hangfire.StructureMap
+﻿namespace Hangfire.StructureMap
 {
+    using System;
+
+    using global::StructureMap;
+
+    /// <summary>
+    /// Bootstrapper Configuration Extensions for StructureMap.
+    /// </summary>
     public static class StructureMapBootstrapperConfigurationExtensions
     {
         /// <summary>
         /// Tells bootstrapper to use the specified StructureMap container as a global job activator.
         /// </summary>
-        /// <param name="configuration">Configuration</param>
+        /// <param name="configuration">Bootstrapper Configuration</param>
         /// <param name="container">StructureMap container that will be used to activate jobs</param>
+        [Obsolete("Please use `GlobalConfiguration.Configuration.UseStructureMapActivator` method instead. Will be removed in version 2.0.0.")]
         public static void UseStructureMapActivator(this IBootstrapperConfiguration configuration, IContainer container)
         {
             configuration.UseActivator(new StructureMapJobActivator(container));
