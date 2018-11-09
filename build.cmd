1aa626b15561e462cb1922c26c9026e5bd4b54ab
cocowalla <800977+cocowalla@users.noreply.github.com>
2018-11-09 15:53:10 +0000

    Build scripts and CI config

--- build.cmd
+++ build.cmd
@@ -1,3 +1,15 @@
-@echo off
+set START_DIR=%cd%
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build\Hangfire.StructureMap.proj /v:m /p:Version=%1
+dotnet restore .\Hangfire.StructureMap.sln
+dotnet build .\src\Hangfire.StructureMap\Hangfire.StructureMap.csproj --configuration Release
+
+rem dotnet-xunit is a CLI tool that can only be executed from in the test folder
+cd .\test\Hangfire.StructureMap.Test
+
+dotnet xunit -framework netcoreapp1.1 -fxversion 1.1.7
+dotnet xunit -framework netcoreapp2.0 -fxversion 2.0.6
+dotnet xunit -framework net452
+
+cd %START_DIR%
+
+dotnet pack .\src\Hangfire.StructureMap -c Release
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ev
+
+dotnet restore ./Hangfire.StructureMap.sln
+dotnet build ./src/Hangfire.StructureMap/Hangfire.StructureMap.csproj --configuration Release
+
+# dotnet-xunit is a CLI tool that can only be executed from in the test folder
+cd ./test/Hangfire.StructureMap.Test
+
+dotnet xunit -framework netcoreapp1.1 -fxversion 1.1.7
+dotnet xunit -framework netcoreapp2.0 -fxversion 2.0.9
+
+cd ${TRAVIS_BUILD_DIR}
