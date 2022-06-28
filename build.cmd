b62c481dea390459645b6b1713d2e44e016b2d9b
cocowalla <800977+cocowalla@users.noreply.github.com>
2022-06-28 14:47:25 +0100

    feat: update target frameworks

--- build.cmd
+++ build.cmd
@@ -3,12 +3,7 @@ set START_DIR=%cd%
 dotnet restore .\Hangfire.StructureMap.sln
 dotnet build .\src\Hangfire.StructureMap\Hangfire.StructureMap.csproj --configuration Release
 
-rem dotnet-xunit is a CLI tool that can only be executed from in the test folder
-cd .\test\Hangfire.StructureMap.Test
-
-dotnet xunit -framework netcoreapp1.1 -fxversion 1.1.2
-dotnet xunit -framework netcoreapp2.0 -fxversion 2.0.6
-dotnet xunit -framework net452
+dotnet test
 
 cd %START_DIR%
 
--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ set -ev
 dotnet restore ./Hangfire.StructureMap.sln
 dotnet build ./src/Hangfire.StructureMap/Hangfire.StructureMap.csproj --configuration Release
 
-# dotnet-xunit is a CLI tool that can only be executed from in the test folder
-cd ./test/Hangfire.StructureMap.Test
-
-dotnet xunit -framework netcoreapp1.1 -fxversion 1.1.7
-dotnet xunit -framework netcoreapp2.0 -fxversion 2.0.5
+dotnet test
 
 cd ${TRAVIS_BUILD_DIR}
